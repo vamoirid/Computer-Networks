90a70561bf26e117885dd641035310cdacfcc719
Vasileios Amoiridis <vassilisamir@gmail.com>
2020-04-11 02:50:49 +0300

    Added GPS() method.

--- Serial Communications/src/userApplication/virtualModem.java
+++ Serial Communications/src/userApplication/virtualModem.java
@@ -138,27 +138,27 @@ public class virtualModem {
 		
 		//Method for Image without Errors method
 		public void imageWithoutErrors(Modem modem) throws IOException {
-			int  k, countPixels = 0;
-			ArrayList<Integer> pixelsValue = new ArrayList<Integer>();  
+			int  k, countJPEGdata = 0;											//Counter for the number of messages received
+			ArrayList<Integer> jpegDataValue = new ArrayList<Integer>();  		//ArrayList for storing JPEG data
 			
 			File imageFile = new File("imageWithoutErrors.jpeg");
-			FileOutputStream imageStream = new FileOutputStream(imageFile);
+			FileOutputStream imageStream = new FileOutputStream(imageFile);		//File for saving the image data
 			
 			modem.write(("MXXXX" + "CAM=PTZ" + "DIR=U" + "\r").getBytes());
 			for(;;) {
 				try {
 					k = modem.read();
 					if (k == -1) break;
-					pixelsValue.add(k);
-					countPixels++;
+					jpegDataValue.add(k);										//Storing the info
+					countJPEGdata++;
 				}
 				catch (Exception x) {
 					break;
 				}
 			}
 			
-			for (int i = 0; i < countPixels; i++) {
-				imageStream.write(pixelsValue.get(i));
+			for (int i = 0; i < countJPEGdata; i++) {
+				imageStream.write(jpegDataValue.get(i));						//Saving the image
 			}
 			
 			imageStream.close();	
@@ -166,8 +166,8 @@ public class virtualModem {
 		
 		//Method for Image with Errors method
 		public void imageWithErrors(Modem modem) throws IOException {
-			int  k, countPixels = 0;
-			ArrayList<Integer> pixelsValue = new ArrayList<Integer>();  
+			int  k, countJPEGdata = 0;
+			ArrayList<Integer> jpegDataValue = new ArrayList<Integer>();  
 			
 			File imageFile = new File("imageWithErrors.jpeg");
 			FileOutputStream imageStream = new FileOutputStream(imageFile);
@@ -177,23 +177,170 @@ public class virtualModem {
 				try {
 					k = modem.read();
 					if (k == -1) break;
-					pixelsValue.add(k);
-					countPixels++;
+					jpegDataValue.add(k);
+					countJPEGdata++;
 				}
 				catch (Exception x) {
 					break;
 				}
 			}
 
-			for (int i = 0; i < countPixels; i++) {
-				imageStream.write(pixelsValue.get(i));
+			for (int i = 0; i < countJPEGdata; i++) {
+				imageStream.write(jpegDataValue.get(i));
 			}
 			
 			imageStream.close();
 		} //End of Image with Errors method
 		
 		//Method for GPS data
-		public void GPS(Modem modem) throws IOException, InterruptedException {
+		public void GPS(Modem modem) throws IOException, InterruptedException { 
+			int  k, gpsDataCnt = 0, gpsDataGPGGACnt = 0, gpsImageCnt = 0;
+			String receivedMessage = "";
+			ArrayList<String> gpsData = new ArrayList<String>();
+			ArrayList<String> gpsDataGPGGA = new ArrayList<String>();
+			ArrayList<String> gpsDataGPGGARandom = new ArrayList<String>();
+			
+			File gpsText = new File("GPS.txt");
+			File gpsImage = new File("GPS.jpeg");
+			FileOutputStream gpsTextStream = new FileOutputStream(gpsText);
+			FileOutputStream gpsImageStream = new FileOutputStream(gpsImage);
+			
+			modem.write(("PXXXX" + "R=1003297" +"\r").getBytes());
+			for(;;) {
+				try {
+					k = modem.read();
+					if (k == -1) break;
+					receivedMessage += (char)k;
+					System.out.print((char)k);
+				}
+				catch (Exception x) {
+					break;
+				}
+				if (receivedMessage.indexOf("\r\n") > -1) {
+					gpsData.add(receivedMessage);
+					gpsDataCnt++;
+					
+					if (receivedMessage.indexOf("GPGGA") > -1) {
+						//System.out.print(receivedMessage);
+						gpsDataGPGGA.add(receivedMessage);
+						gpsDataGPGGACnt++;
+					}
+					receivedMessage = "";
+				}
+			}
+			System.out.println("Total received GPGGA: " + gpsDataGPGGACnt);
+			for (int i = 0; i < gpsDataCnt; i++) {
+				gpsTextStream.write(gpsData.get(i).getBytes());
+			}
+			
+			gpsTextStream.close();
+			
+		    int min = 1, max = 11;
+			String geographicLongitude = "";
+			String geographicLatitude = "";
+			String degreesLongitude = "";
+			String degreesLatitude = "";
+			String minutesLongitude = "";
+			String minutesLatitude = "";
+			String minutesDecimalLongitude = "";
+			String minutesDecimalLatitude = "";
+			String secondsLongitude = "";
+			String secondsLatitude = "";
+			int indexOfDecimal; //index of decimal number for decimal conversion
+			
+			ArrayList<String> tValues = new ArrayList<String>();
+			//Start picking 9 random GPGGA messages
+			for (int i = 0; i < 9; i++) { // 9 loops
+				int rand_int = (int)(Math.random()*(max - min + 1) + min);
+				min += 9;
+				max += 9;
+				gpsDataGPGGARandom.add(gpsDataGPGGA.get(rand_int - 1)); //rand_int - 1 or i
+				System.out.println(rand_int);
+				System.out.print(gpsDataGPGGARandom.get(i));
+				System.out.println("Length of String = " + gpsDataGPGGARandom.get(i).length());
+				//18 19 20 21 23 24
+				degreesLatitude += (char)gpsDataGPGGARandom.get(i).charAt(18);
+				degreesLatitude += (char)gpsDataGPGGARandom.get(i).charAt(19);
+				minutesLatitude += (char)gpsDataGPGGARandom.get(i).charAt(20);
+				minutesLatitude += (char)gpsDataGPGGARandom.get(i).charAt(21);
+				minutesDecimalLatitude += (char)gpsDataGPGGARandom.get(i).charAt(23);
+				minutesDecimalLatitude += (char)gpsDataGPGGARandom.get(i).charAt(24);
+				minutesDecimalLatitude += (char)gpsDataGPGGARandom.get(i).charAt(25);
+				minutesDecimalLatitude += (char)gpsDataGPGGARandom.get(i).charAt(26);
+				
+				//Convert minutesDecimal to seconds
+				double tempSecondsLatitude = (double)Integer.parseInt(minutesDecimalLatitude)*60/10000;
+				System.out.println("Double value is: " + tempSecondsLatitude);
+				secondsLatitude = String.valueOf(tempSecondsLatitude);
+				indexOfDecimal = secondsLatitude.indexOf(".");
+				secondsLatitude = secondsLatitude.substring(0, indexOfDecimal);
+				System.out.println("Integer value is: " + secondsLatitude);
+				geographicLatitude = degreesLatitude + minutesLatitude + secondsLatitude;
+				System.out.println("Geographic Latitude = " + geographicLatitude);
+				//End of converting minutesDecimal to seconds
+				
+				degreesLatitude = "";
+				minutesLatitude = "";
+				secondsLatitude = "";
+				minutesDecimalLatitude = "";
+				
+				//Same for Longitude
+				degreesLongitude += (char)gpsDataGPGGARandom.get(i).charAt(31);
+				degreesLongitude += (char)gpsDataGPGGARandom.get(i).charAt(32);
+				minutesLongitude += (char)gpsDataGPGGARandom.get(i).charAt(33);
+				minutesLongitude += (char)gpsDataGPGGARandom.get(i).charAt(34);
+				minutesDecimalLongitude += (char)gpsDataGPGGARandom.get(i).charAt(36);
+				minutesDecimalLongitude += (char)gpsDataGPGGARandom.get(i).charAt(37);
+				minutesDecimalLongitude += (char)gpsDataGPGGARandom.get(i).charAt(38);
+				minutesDecimalLongitude += (char)gpsDataGPGGARandom.get(i).charAt(39);
+				
+				double tempSecondsLongitude = (double)Integer.parseInt(minutesDecimalLongitude)*60/10000;
+				System.out.println("Double value is: " + tempSecondsLongitude);
+				secondsLongitude = String.valueOf(tempSecondsLongitude);
+				indexOfDecimal = secondsLongitude.indexOf(".");
+				secondsLongitude = secondsLongitude.substring(0, indexOfDecimal);
+				System.out.println("Integer value is: " + secondsLongitude);
+				geographicLongitude = degreesLongitude + minutesLongitude + secondsLongitude;
+				System.out.println("Geographic Longitude = " + geographicLongitude);
+				
+				degreesLongitude = "";
+				minutesLongitude = "";
+				secondsLongitude = "";
+				minutesDecimalLongitude = "";
+				
+				tValues.add("T=" + geographicLongitude + geographicLatitude);
+				System.out.println("Olo mazi = " + tValues.get(i));
+				geographicLongitude = "";
+				geographicLatitude = "";
+			}
+			
+			String receivedMessageImage = "";
+			ArrayList<Integer> gpsPixelsValue = new ArrayList<Integer>();
+			
+			String pCodeToSend = "PXXXX";
+			for (int i = 0; i < 9; i++) {
+				pCodeToSend += tValues.get(i);
+			}
+			pCodeToSend += "\r\n";
+			System.out.println(pCodeToSend);
+			modem.write(pCodeToSend.getBytes());
+			for(;;) {
+				try {
+					k = modem.read();
+					if (k == -1) break;
+					gpsPixelsValue.add(k);
+					gpsImageCnt++;
+				}
+				catch (Exception x) {
+					System.out.println(x);
+				}
+			}
+			
+			for(int i = 0; i < gpsImageCnt; i++) {
+				gpsImageStream.write(gpsPixelsValue.get(i));
+			}
+			
+			gpsImageStream.close();
 			
 		} //End of GPS data
 		
